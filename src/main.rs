48b4d12be3b18e2dfd141b44f731b2c47c8cec00
Robin <robin.tordly@nav.no>
2024-11-06 22:14:04 +0100

    telemetry

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,31 @@
-use std::net::TcpListener;
-
 use sqlx::PgPool;
+use std::net::TcpListener;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::{EnvFilter, Registry};
 use zero2prod::config::get_config;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let config = get_config()
-        .expect("Failed to read configuration");
+    init_trace();
 
+    let config = get_config().expect("Failed to read configuration");
     let connection_pool = PgPool::connect(&config.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
-
     let address = format!("127.0.0.1:{}", config.application_port);
-
-    let listener = TcpListener::bind(address)
-        .expect("Failed to bind random port");
-
+    let listener = TcpListener::bind(address).expect("Failed to bind random port");
     run(listener, connection_pool)?.await
 }
 
+fn init_trace() {
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+    set_global_default(subscriber).expect("Failed to set subscriber");
+}
